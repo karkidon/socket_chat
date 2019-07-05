caed7c63148ca8cf10f6d099a791819733314c1b
karkidon <karkidon@icloud.com>
2019-07-05 10:55:42 +0500

    Add files via upload

--- make.sh
+++ make.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo Compiling server
-g++ server.cpp -o server
+g++ server.cpp -o server -lcrypto++
 echo Compiling client
-g++ client.cpp -o client
+g++ client.cpp -o client -lcrypto++
